0369928242b1d194c087bcd7d1c379bf0b553fe2
BenPiggot <ben@Bens-MacBook-Pro.local>
2023-04-05 22:34:11 -0700

    adding first component

---
+++ Shared/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace BlazorEcommerce.Shared
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
+        public string ImageUrl { get; set; } = String.Empty;
+        public decimal Price { get; set; }
+    }
+}
+
